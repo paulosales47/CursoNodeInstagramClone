a95a73016229cf388a7effefaa4cfb884bcb236a
Paulo Henrique Sales Sampaio <paulohenrique.sales47@gmail.com>
2019-02-10 12:47:05 -0200

    Instalando módulo cors para resolver problema de Cross-Origin Resource Sharing

--- api/config/server.js
+++ api/config/server.js
@@ -3,11 +3,20 @@ let express = require('express');
 let bodyParser = require('body-parser');
 let consign = require('consign');
 let connectMultipart = require('connect-multiparty');
+let cors = require('cors');
 let app = express();
+
+let corsConfig = cors({
+    origin: 'http://localhost:3006',
+    optionsSuccessStatus: 200
+})
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(connectMultipart());
+app.use(corsConfig);
+
+app.set('cors', corsConfig);
 
 consign()
     .include('./app/controllers')
