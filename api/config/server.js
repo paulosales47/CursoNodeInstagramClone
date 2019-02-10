fb7c997ef5660c25610c09a7b85fe127cbc909a2
Paulo Henrique Sales Sampaio <paulohenrique.sales47@gmail.com>
2019-02-10 00:32:17 -0200

    Instalando módulo connect-multiparty para trabalhar requisições multipart/form-data

--- api/config/server.js
+++ api/config/server.js
@@ -2,11 +2,12 @@ const PORTA = 3005;
 let express = require('express');
 let bodyParser = require('body-parser');
 let consign = require('consign');
-
+let connectMultipart = require('connect-multiparty');
 let app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+app.use(connectMultipart());
 
 consign()
     .include('./app/controllers')
