2452d4d57d18a9315be690be856a868fe12afa39
Paulo Henrique Sales Sampaio <paulohenrique.sales47@gmail.com>
2019-02-15 22:06:42 -0200

    Configurando site para acessar a API HTTPS

--- api/api.js
+++ api/api.js
@@ -1,7 +1,6 @@
 let {server} = require('./config/server');
 let {app} = require('./config/server');
 
-console.log(app);
 let porta = app.api.config.configuracao.portaAPI;
 
 server.listen(porta, function(){
